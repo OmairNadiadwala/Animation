350c420e174b22fccb4d66f3135e5515e84b45ac
Muhammad Omair <m.omair@kcompute.com>
2015-11-23 19:43:19 +0500

    Animation

---
+++ CustomView.h
@@ -0,0 +1,23 @@
+//
+//  CustomView.h
+//
+//  Code generated using QuartzCode 1.38.4 on 11/11/2015.
+//  www.quartzcodeapp.com
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface CustomView : UIView
+
+@property (nonatomic, strong) UIColor * grayColor;
+@property (nonatomic, strong) UIColor * progressColor;
+@property (nonatomic, strong) UIColor * finishColor;
+@property (nonatomic, assign) CGFloat  oldAnimProgress;
+
+- (void)addOldAnimation;
+- (void)addOldAnimationCompletionBlock:(void (^)(BOOL finished))completionBlock;
+- (void)removeAnimationsForAnimationId:(NSString *)identifier;
+- (void)removeAllAnimations;
+
+@end
